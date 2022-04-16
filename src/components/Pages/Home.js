14377ef03b4993a5a3d9b3ffb13ff3e128ecb6f3
klaercio <klaerciosilva@gmail.com>
2022-04-16 13:21:15 -0300

    Seção home finalizada

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,4 +1,14 @@
+import styles from './Home.module.css';
+import savings from '../../img/savings.svg';
+import LinkButton from '../layouts/LinkButton.js';
+
 export default function Home() {
     return (
-    <h1>Home</h1>);
+        <section className={styles.home__container}>
+            <h1>Bem vindo ao <span>Costs</span></h1>
+            <p>Começa a gerenciar seus projetos agora mesmo!</p>
+            <LinkButton to='/NewProject' text="Criar projeto"></LinkButton>
+            <img src={savings} alt="Costs"/>
+        </section>    
+    );
 }
---
+++ src/components/layouts/LinkButton.js
@@ -0,0 +1,10 @@
+import styles from './LinkButton.module.css';
+import {Link} from 'react-router-dom';
+
+export default function LinkButton({to, text}) {
+    return <>
+        <Link to={to} className={styles.btn}>
+            {text}
+        </Link>
+    </>
+}
